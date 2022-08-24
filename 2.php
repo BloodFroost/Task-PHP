454d580f35b818ce354a0fd1039272d8f234da9f
BloodFroost <78503517+BloodFroost@users.noreply.github.com>
2022-08-24 21:06:13 +0300

    Реализована функция mySortForKey($a, $b).

--- 2.php
+++ 2.php
@@ -1,4 +1,7 @@
 <?php
+// convertString($a, $b). Результат ее выполнение: если в строке $a содержится 2 и более подстроки $b,
+// то во втором месте заменить подстроку $b на инвертированную подстроку.
+
 function convertString($a, $b)
 {
     $substringСheck = (mb_substr_count($a, $b));
@@ -15,3 +18,29 @@ function convertString($a, $b)
 // $a = "string substring string substring string substring";
 // $b = "substring";
 //print_r(convertString($a, $b));
+
+// c. $a – двумерный массив вида [['a'=>2,'b'=>1],['a'=>1,'b'=>3]], 
+// $b – ключ вложенного массива.
+// Результат ее выполнения: двумерном массива $a отсортированный по возрастанию значений для ключа $b. 
+// В случае отсутствия ключа $b в одном из вложенных массивов, выбросить ошибку класса Exception с индексом неправильного массива.
+
+
+function mySortForKey($a, $b)
+{
+    foreach ($a as $key => $value) {
+        if ($value[$b] == null) {
+            throw new Exception('Индекс масссива неверный: ' . $a[$value][$key]);
+        } else {
+            $keys = array_column($a, $b);
+            $sort = array_multisort($keys, SORT_ASC, $a);
+            return $a;
+        }
+    }
+}
+$a = [
+    ['a' => 2, 'b' => 3],
+    ['a' => 3, 'b' => 5],
+    ['a' => 1, 'b' => 2]
+];
+$b='b';
+print_r(mySortForKey($a,$b));
